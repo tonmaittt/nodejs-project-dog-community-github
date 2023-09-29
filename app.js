8701d01f44cfeef9c09a67c9073df75618cf8530
Thirdvespa <68259319+Thirdvespa@users.noreply.github.com>
2023-09-29 14:18:56 +0700

    edit route login pls

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ let connection = require('./lib/db');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var userDataRouter = require('./routes/userData');
+var loginPage = require('./route/login')
+
 
 var app = express();
 
@@ -38,6 +40,7 @@ app.use(flash());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/userData', userDataRouter);
+app.use('/login', loginPage);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,5 +57,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// route page
+app.get('/', (req, res) => {
+  res.render('pages/login.ejs');
+ });
 
 module.exports = app;
