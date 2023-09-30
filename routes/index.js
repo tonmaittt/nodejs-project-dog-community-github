164c684f832d316ac8699f8673d0d5e25b4840dd
NewBie <mai-2543@hotmail.com>
2023-09-30 20:19:59 +0700

    manu userData

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+let dbCon = require("../lib/db");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 /* login page. */
-router.get('/login', function(req, res, next) {
-  res.render('loginUser', { title: 'Express' });
+router.get("/login", function (req, res, next) {
+  res.render("loginUser", { title: "Express" });
 });
 
 /* regis page. */
-router.get('/regis', function(req, res, next) {
-  res.render('regisUser', { title: 'Express' });
+router.get("/regis", function (req, res, next) {
+  res.render("regisUser", { title: "Express" });
+});
+
+/* userData page. */
+router.get("/user", function (req, res, next) {
+  dbCon.query("SELECT * FROM tb_user ORDER BY id asc", (err, rows) => {
+    if (err) {
+      req.flash("error", err);
+      res.render("userData", { data: "" });
+    } else {
+      res.render("userData", { data: rows });
+    }
+  });
 });
 
 module.exports = router;
